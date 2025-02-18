a25a15776cf7b1948d63db9693831766e08c3014
yxkong <5ycode@sina.com>
2025-02-18 15:05:33 +0800

    feat: 增加异常处理

--- platform-core-standard/src/main/java/com/github/platform/core/standard/entity/event/DomainEvent.java
+++ platform-core-standard/src/main/java/com/github/platform/core/standard/entity/event/DomainEvent.java
@@ -46,9 +46,11 @@ public class DomainEvent implements Serializable {
     private String msg;
     @Schema(description = "消息的租户")
     private Integer tenantId;
-
+    @Builder.Default
     private UserInfo userInfo = new UserInfo();
+    @Builder.Default
     private AppInfo appInfo = new AppInfo();
+    @Builder.Default
     private HandlerInfo handlerInfo = new HandlerInfo();
 
 
